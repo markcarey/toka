22c26f541673b27fd05f4a1db116b699e31088ec
markcarey <mark@markcarey.com>
2024-03-22 13:53:28 -0400

    mintWithDegen contract progress

--- contracts/MintWithDegen.sol
+++ contracts/MintWithDegen.sol
@@ -4,119 +4,85 @@ pragma solidity ^0.8.17;
 
 import "./interfaces/IZoraCreatorFixedPriceSaleStrategy.sol";
 import "./interfaces/IZoraCreatorMerkleMinterStrategy.sol";
-import {IZoraCreator1155Factory} from "@zoralabs/zora-1155-contracts/src/interfaces/IZoraCreator1155Factory.sol";
-import {IZoraCreator1155} from "@zoralabs/zora-1155-contracts/src/interfaces/IZoraCreator1155.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./interfaces/IZoraCreator1155.sol";
+import "./interfaces/IERC721Drop.sol";
+//import {IZoraCreator1155Factory} from "@zoralabs/zora-1155-contracts/src/interfaces/IZoraCreator1155Factory.sol";
+//import {IZoraCreator1155} from "@zoralabs/zora-1155-contracts/src/interfaces/IZoraCreator1155.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MintWithDegen {
+contract MintWithDegen is AccessControl {
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     // degen token address on Base:
     IERC20 public _degen;
-    // zora factory address:
-    IZoraCreator1155Factory public _zoraFactory = IZoraCreator1155Factory(0x777777C338d93e2C7adf08D102d45CA7CC4Ed021);
+    // zora factory address TODO: do we need the factory?
+    //IZoraCreator1155Factory public _zoraFactory = IZoraCreator1155Factory(0x777777C338d93e2C7adf08D102d45CA7CC4Ed021);
+    IZoraCreatorFixedPriceSaleStrategy public _fixedPriceSaleStrategy;
+    IZoraCreatorMerkleMinterStrategy public _merkleMinterStrategy;
+    uint256 public _mintFee; // mint fee in degen
+    uint256 public _tokaFee; // toka's take in degen
 
-    constructor(IERC20 degen) {
+    // degen price per token per contract address: contract => tokenId => price
+    mapping(address => mapping(uint256 => uint256)) public _degenPricePerToken;
+
+    constructor(IERC20 degen, IZoraCreatorFixedPriceSaleStrategy fixedPriceSaleStrategy, IZoraCreatorMerkleMinterStrategy merkleMinterStrategy, uint256 mintFee, uint256 tokaFee) {
         _degen = degen;
+        _fixedPriceSaleStrategy = fixedPriceSaleStrategy;
+        _merkleMinterStrategy = merkleMinterStrategy;
+        _mintFee = mintFee;
+        _tokaFee = tokaFee;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MANAGER_ROLE, msg.sender);
     }
 
     //mint function
-    function mint(uint256 tokenId, uint256 amount) public {
-        //get the zora contract address
-        address zoraContract = _zoraFactory.addresses(tokenId);
-        //get the zora contract
-        IZoraCreator1155 zora = IZoraCreator1155(zoraContract);
+    function mintWithDegen1155(address to, IZoraCreator1155 nft, uint256 tokenId, uint256 amount) public {
+        //get the zora nft contract
+        //IZoraCreator1155 nft = IZoraCreator1155(nftContract);
         //get the sale strategy
-        (address saleStrategy, uint256 saleStrategyId) = zora.saleStrategy(tokenId);
-        //get the sale strategy contract
-        IZoraCreatorFixedPriceSaleStrategy fixedPriceSaleStrategy = IZoraCreatorFixedPriceSaleStrategy(saleStrategy);
-        //get the sale strategy contract
-        IZoraCreatorMerkleMinterStrategy merkleMinterStrategy = IZoraCreatorMerkleMinterStrategy(saleStrategy);
-        //get the sale strategy contract
-        IZoraCreatorMerkleMinterStrategy.MerkleSaleSettings memory merkleSaleSettings = merkleMinterStrategy.sale(zoraContract, tokenId);
-        //get the sale strategy contract
-        IZoraCreatorFixedPriceSaleStrategy.SalesConfig memory salesConfig = fixedPriceSaleStrategy.sale(zoraContract, tokenId);
+        IZoraCreatorFixedPriceSaleStrategy.SalesConfig memory salesConfig = _fixedPriceSaleStrategy.sale(address(nft), tokenId);
         //check if the sale is a fixed price sale
         if (salesConfig.saleStart > 0) {
             //check if the sale is still ongoing
             require(block.timestamp >= salesConfig.saleStart && block.timestamp <= salesConfig.saleEnd, "Sale not ongoing");
-            //check if the user has enough degen
-            require(_degen.balanceOf(msg.sender) >= salesConfig.pricePerToken * amount, "Not enough degen");
-            //transfer the degen to the contract
-            _degen.transferFrom(msg.sender, address(this), salesConfig.pricePerToken * amount);
-            //mint the token
-            zora.mint(msg.sender, tokenId, amount, "");
-        } else {
-            //check if the sale is still ongoing
-            require(block.timestamp >= merkleSaleSettings.presaleStart && block.timestamp <= merkleSaleSettings.presaleEnd, "Sale not ongoing");
-            //check if the user has enough degen
-            require(_degen.balanceOf(msg.sender) >= salesConfig.pricePerToken * amount, "Not enough degen");
-            //transfer the degen to the contract
-            _degen.transferFrom(msg.sender, address(this), salesConfig.pricePerToken * amount);
-            //mint the token
-            zora.mint(msg.sender, tokenId, amount, "");
         }
+        // check if amount is less than max tokens per address
+        require(salesConfig.maxTokensPerAddress == 0 || nft.balanceOf(msg.sender, tokenId) + amount <= salesConfig.maxTokensPerAddress, "Max tokens per address reached");
+
+        //check if the user has enough degen
+        //require(_degen.balanceOf(msg.sender) >= salesConfig.pricePerToken * amount, "Not enough degen");
+
+        //transfer the degen to the contract
+        _degen.transferFrom(msg.sender, address(this), ( _mintFee + _degenPricePerToken[address(nft)][tokenId] ) * amount);
+        //mint the token
+        nft.adminMint(to, tokenId, amount, "");
+        // transfer degen and fees
+        _degen.transfer(salesConfig.fundsRecipient, _degenPricePerToken[address(nft)][tokenId] * amount);
     }
 
-    /// @notice Compiles and returns the commands needed to mint a token using this sales strategy
-    /// @param mintMsgSender The address that called the mint on the 1155 contract
-    /// @param tokenId The token ID to mint
-    /// @param quantity The quantity of tokens to mint
-    /// @param ethValueSent The amount of ETH sent with the transaction
-    /// @param minterArguments The arguments passed to the minter, which should be the address to mint to
-    function requestMint(
-        address mintMsgSender,
-        uint256 tokenId,
-        uint256 quantity,
-        uint256 ethValueSent,
-        bytes calldata minterArguments
-    ) external returns (ICreatorCommands.CommandSet memory commands) {
-        // Ensure the minter is allowed to mint either this token or all tokens contract-wide
-        if (!isMinter(msg.sender, tokenId, mintMsgSender)) {
-            revert ONLY_MINTER();
-        }
-
-        address mintTo;
-        string memory comment = "";
-
-        if (minterArguments.length == 32) {
-            mintTo = abi.decode(minterArguments, (address));
-        } else {
-            (mintTo, comment) = abi.decode(minterArguments, (address, string));
-        }
-
-        SalesConfig storage config = salesConfigs[msg.sender][tokenId];
-
-        // If sales config does not exist this first check will always fail.
-
-        // Check sale end
-        if (block.timestamp > config.saleEnd) {
-            revert SaleEnded();
-        }
-
-        // Check sale start
-        if (block.timestamp < config.saleStart) {
-            revert SaleHasNotStarted();
-        }
-
-        // Check value sent
-        if (config.pricePerToken * quantity != ethValueSent) {
-            revert WrongValueSent();
-        }
-
-        bool shouldTransferFunds = config.fundsRecipient != address(0);
-        commands.setSize(shouldTransferFunds ? 2 : 1);
-
-        // Mint command
-        commands.mint(mintTo, tokenId, quantity);
-
-        if (bytes(comment).length > 0) {
-            emit MintComment(mintTo, msg.sender, tokenId, quantity, comment);
-        }
-
-        // Should transfer funds if funds recipient is set to a non-default address
-        if (shouldTransferFunds) {
-            commands.transfer(config.fundsRecipient, ethValueSent);
-        }
+    function setDegenPricePerToken1155(IZoraCreator1155 nft, uint256 tokenId, uint256 price) public {
+        // require admin permissions on NFT contract
+        require(nft.isAdminOrRole(msg.sender, tokenId, nft.PERMISSION_BIT_MINTER()), "Not admin");
+        _degenPricePerToken[address(nft)][tokenId] = price;
     }
-    
+
+    // set mint fee function onlyRole(MANAGER_ROLE)
+    function setMintFee(uint256 mintFee) public {
+        require(hasRole(MANAGER_ROLE, msg.sender), "Not manager");
+        _mintFee = mintFee;
+    }
+
+    // set toka fee function
+    function setTokaFee(uint256 tokaFee) public {
+        require(hasRole(MANAGER_ROLE, msg.sender), "Not manager");
+        _tokaFee = tokaFee;
+    }
+
+    // withdraw function for toka admin
+    function withdrawDegen() public {
+        require(hasRole(MANAGER_ROLE, msg.sender), "Not manager");
+        _degen.transfer(msg.sender, _degen.balanceOf(address(this)));
+    }
+
 }
 
---
+++ contracts/interfaces/IZoraCreator1155.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IZoraCreator1155 {
+    
+    function adminMint(address recipient, uint256 tokenId, uint256 quantity, bytes memory data) external;
+
+    function adminMintBatch(address recipient, uint256[] memory tokenIds, uint256[] memory quantities, bytes memory data) external;
+
+    function burnBatch(address user, uint256[] calldata tokenIds, uint256[] calldata amounts) external;
+
+    function balanceOf(address account, uint256 id) external view returns (uint256);
+
+    function PERMISSION_BIT_MINTER() external view returns (uint256);
+    function isAdminOrRole(address user, uint256 tokenId, uint256 role) external view returns (bool);
+
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+
+}
